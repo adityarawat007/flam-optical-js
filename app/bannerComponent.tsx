1fbd10cd98ba5184554949b87464c390879e7e8e
Aditya Rawat <rawataditya795@gmail.com>
2025-03-05 17:34:28 +0530

    feat: implement banner visibility control and onboarding welcome section (#2)

--- app/bannerComponent.tsx
+++ app/bannerComponent.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
+import React, { useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
@@ -13,12 +14,33 @@ import Image from "next/image";
 
 export const BannerComponent = ({
   setOnboading,
+  showBanner,
+  setShowBanner
 }: {
+  showBanner: boolean;
   setOnboading: (value: boolean) => void;
+  setShowBanner: (value: boolean) => void;
 }) => {
+
+  const sheetRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (sheetRef.current && !sheetRef.current.contains(event.target as Node)) {
+        setShowBanner(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sheetRef, setShowBanner]);
+
   return (
-    <Sheet open={true}>
+    <Sheet open={showBanner} >
       <SheetContent
+      ref={sheetRef}
         side="bottom"
         className=" border-0 max-w-3xl my-2 mx-auto select-none bg-transparent backdrop-blur-3xl rounded-4xl w-full "
       >
